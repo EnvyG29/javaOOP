4e094f733d28f5154ea4be1bda6d0ca3b7387675
EnvyG <3x92b6@gmail.com>
2023-06-04 15:41:14 +0300

    dz3MyLinkedList

--- src/main/java/dz/dz3MyClass/Main.java
+++ src/main/java/dz/dz3MyClass/Main.java
@@ -14,28 +14,28 @@ public class Main {
         list.add("первый элемент");
         list.add("первый элемент");
         list.add("первый элемент");
-        for (String item: list) {
+        for (String item : list) {
             System.out.println(item);
         }
 
         list.remove("первый элемент", false);
         System.out.println("\n__remove__");
         iterator = list.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
 
 
         list.remove("первый элемент", true);
         System.out.println("\n__remove__all");
-        for (String item: list) {
+        for (String item : list) {
             System.out.println(item);
         }
-        System.out.printf("длина списка - %d\n",list.size());
+        System.out.printf("длина списка - %d\n", list.size());
 
         list.addFirst("новый элемент");
         System.out.println("\n--addFirst--");
-        for (String item: list) {
+        for (String item : list) {
             System.out.println(item);
         }
 
@@ -43,20 +43,20 @@ public class Main {
         System.out.println(list.get(2));
         System.out.println(list.get(-2));
 
-        list.removeFirst();;
+        list.removeFirst();
         System.out.println("\n--removeFirst--");
-        for (String item: list) {
+        for (String item : list) {
             System.out.println(item);
         }
-        System.out.printf("длина списка - %d\n",list.size());
+        System.out.printf("длина списка - %d\n", list.size());
 
-        list.removeLast();;
+        list.removeLast();
         System.out.println("\n--removeLast--");
         iterator = list.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
-        System.out.printf("длина списка - %d\n",list.size());
+        System.out.printf("длина списка - %d\n", list.size());
 
         list.add("первый элемент");
         list.add("второй элемент");
@@ -67,17 +67,17 @@ public class Main {
         list.add("первый элемент");
         list.add("четвертый элемент");
         System.out.println("\n---пополнили список");
-        for (String item: list) {
+        for (String item : list) {
             System.out.println(item);
         }
 
-        list.clear();;
+        list.clear();
         System.out.println("\n--clear--");
         iterator = list.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
-        System.out.printf("длина списка - %d\n",list.size());
+        System.out.printf("длина списка - %d\n", list.size());
 
         System.out.println("есть значения в списке? - " + list.isEmpty());
 
@@ -86,10 +86,10 @@ public class Main {
         list.add("шестой элемент");
         list.add("седьмой элемент");
         System.out.println("\n---пополнили список");
-        for (String item: list) {
+        for (String item : list) {
             System.out.println(item);
         }
-        System.out.printf("длина списка - %d\n",list.size());
+        System.out.printf("длина списка - %d\n", list.size());
         System.out.println("есть значения в списке? - " + list.isEmpty());
 
         list.set(2, "элемент");
@@ -97,7 +97,7 @@ public class Main {
 
 
         iterator = list.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
     }
